a7e97d4c256f14173de46bc979f4290bdf0044f2
Raniel Lopes <raniel.ldms@gmail.com>
2024-11-08 12:22:18 -0300

    Finalizando o projeto

---
+++ script.js
@@ -0,0 +1,49 @@
+async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+  try {
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    var consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+    var bairro = document.getElementById("bairro");
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+    bairro.value = consultaCEPConvertida.bairro;
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+    console.log(erro);
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+/* --- Lidando com várias requisições ao mesmo tempo com Promise.all ---
+
+let ceps = ["01001000", "01001001"];
+let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
+*/
+
+//Metódo antigo usando .then
+
+/* .then((resposta) => resposta.json())
+  .then((r) => {
+    if (r.erro) {
+      throw Error("Esse cep não existe!");
+    } else console.log(r);
+  })
+  .catch((erro) => console.log(erro))
+  .finally((mensagem) => console.log("Processsamento concluído!"));
+*/
